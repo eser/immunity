0f5997da0ec86147b78132939b28f64d693754c0
Eser Ozvataf <eser@ozvataf.com>
2016-11-06 22:54:12 +0300

    Added split, take, drop methods.

--- src/Immunity.js
+++ src/Immunity.js
@@ -38,6 +38,92 @@ export class Immunity {
         return Object.assign({}, ...values, instance);
     }
 
+    splitArray(instance, n) {
+        const offset = (n >= 0) ? n : instance.length + n;
+
+        return {
+            items: instance.slice(0, offset),
+            remainder: instance.slice(offset)
+        };
+    }
+
+    splitObject(instance, n) {
+        const keys = Object.keys(instance),
+            offset = (n >= 0) ? n : keys.length + n;
+
+        let index = 0;
+
+        return keys.reduce(
+            (obj, itemKey) => {
+
+                if (index < offset) {
+                    index += 1;
+
+                    return {
+                        items: Object.assign({}, obj.items, { [itemKey]: instance[itemKey] }),
+                        remainder: obj.remainder
+                    };
+                }
+
+                return {
+                    items: obj.items,
+                    remainder: Object.assign({}, obj.remainder, { [itemKey]: instance[itemKey] })
+                };
+            },
+            {
+                items: [],
+                remainder: []
+            }
+        );
+    }
+
+    takeFromArray(instance, n) {
+        return instance.slice(0, n);
+    }
+
+    takeFromObject(instance, n) {
+        let index = 0;
+
+        return Object.keys(instance).reduce(
+            (obj, itemKey) => {
+
+                if (index < n) {
+                    index += 1;
+
+                    return Object.assign({}, obj, { [itemKey]: instance[itemKey] });
+                }
+
+                return obj;
+            },
+            {}
+        );
+    }
+
+    dropFromArray(instance, n) {
+        return instance.slice(instance.length - n);
+    }
+
+    dropFromObject(instance, n) {
+        const keys = Object.keys(instance),
+            offset = keys.length - n;
+
+        let index = 0;
+
+        return Object.keys(instance).reduce(
+            (obj, itemKey) => {
+
+                if (index >= offset) {
+                    return Object.assign({}, obj, { [itemKey]: instance[itemKey] });
+                }
+
+                index += 1;
+
+                return obj;
+            },
+            {}
+        );
+    }
+
     filterArray(instance, predicate) {
         return instance.filter(predicate);
     }
