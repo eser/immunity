c92f22d331e7af72884d127c91d740572e613d46
Quentin Decaunes <le.storm1er@gmail.com>
2018-03-07 17:06:47 +0100

    Fix ES5 compatibility : run tsc.

--- packages/00_ponyfills/lib/assign.js
+++ packages/00_ponyfills/lib/assign.js
@@ -1,14 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function assignPolyfill(target, ...sources) {
-    for (const source of sources) {
-        for (const key of Object.getOwnPropertyNames(source)) {
+function assignPolyfill(target) {
+    var sources = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        sources[_i - 1] = arguments[_i];
+    }
+    for (var _a = 0, sources_1 = sources; _a < sources_1.length; _a++) {
+        var source = sources_1[_a];
+        for (var _b = 0, _c = Object.getOwnPropertyNames(source); _b < _c.length; _b++) {
+            var key = _c[_b];
             target[key] = source[key];
         }
     }
     return target;
 }
 exports.assignPolyfill = assignPolyfill;
-const assign = (Object.assign !== undefined) ? Object.assign : assignPolyfill;
+var assign = (Object.hasOwnProperty('assign') ? Object['assign'] : assignPolyfill);
 exports.default = assign;
 //# sourceMappingURL=assign.js.map
