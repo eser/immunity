d2f6df34f3f856ecd69c37749c2da985f923a543
Eser Ozvataf <eser@ozvataf.com>
2018-08-03 23:01:49 +0300

    Updated dependencies and made changes on jest.config.js.

--- packages/06_senior/jest.config.js
+++ packages/06_senior/jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
+    "testEnvironment": "node",
     "roots": [
         "<rootDir>/src"
     ],
     "transform": {
         "^.+\\.tsx?$": "ts-jest"
     },
-    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$",
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.([jt]sx?|mjs)$",
     "moduleFileExtensions": [
         "ts",
         "tsx",
